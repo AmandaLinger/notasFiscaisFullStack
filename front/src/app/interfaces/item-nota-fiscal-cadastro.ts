35758ecacc5d59bb81600ca5c76dff5fbabc6fbc
amanda <amandamarqueslinger@gmail.com>
2026-08-04 09:47:48 -0300

    melhorando popUp e campos de input

--- front/src/app/interfaces/item-nota-fiscal-cadastro.ts
+++ front/src/app/interfaces/item-nota-fiscal-cadastro.ts
@@ -1,5 +1,6 @@
 export interface ItemNotaFiscalCadastro{
   produtoId: number;
+  produtoNome?: string;
   quantidade: number;
   precoUnitario: number;
 }
--- front/src/app/pages/nota/nota.ts
+++ front/src/app/pages/nota/nota.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {NotaFiscalService} from '../../services/nota-fiscal-service';
-import {DxDataGridModule, DxSelectBoxModule} from 'devextreme-angular';
+import {DxDataGridModule, DxNumberBoxModule, DxSelectBoxModule, DxTextBoxModule} from 'devextreme-angular';
 import {NotaFiscal} from '../../interfaces/nota-fiscal';
 import {Cliente} from '../../interfaces/cliente';
 import {Produto} from '../../interfaces/produto';
+import {ItemNotaFiscalCadastro} from '../../interfaces/item-nota-fiscal-cadastro';
+import {DxButtonModule} from 'devextreme-angular/ui/button';
 
 @Component({
   selector: 'app-nota',
   standalone: true,
-  imports: [DxDataGridModule, DxSelectBoxModule],
+  imports: [CommonModule, DxDataGridModule, DxSelectBoxModule, DxButtonModule, DxTextBoxModule, DxNumberBoxModule],
   templateUrl: './nota.html',
   styleUrl: './nota.scss',
 })
@@ -19,6 +22,7 @@ export class Nota  implements OnInit {
   notas: NotaFiscal[] = [];
   clientes: Cliente[] = [];
   produtos: Produto[] = [];
+  itens: ItemNotaFiscalCadastro[] = [];
 
   ngOnInit() {
     this.notaFiscalService.listar().subscribe((data) => {
@@ -32,5 +36,19 @@ export class Nota  implements OnInit {
     this.notaFiscalService.listarProdutos().subscribe((data) => {
       this.produtos = data;
     });
+  }
+
+
+  excluirLinha(index: number) {
+    this.itens.splice(index, 1);
+  }
+
+  adicionarItem(): void {
+    this.itens.push({
+      produtoId: 0,
+      produtoNome: '',
+      quantidade: 1,
+      precoUnitario: 0
+    })
   }
 }
