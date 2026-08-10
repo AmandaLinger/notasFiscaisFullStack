511e1493b3315aff6ae4b51782f0b10ad31005f5
amanda <amandamarqueslinger@gmail.com>
2026-08-10 14:30:13 -0300

    retirando imports não usados

--- front/src/app/shared/components/cidade-estado/cidade-estado.ts
+++ front/src/app/shared/components/cidade-estado/cidade-estado.ts
@@ -1,8 +1,6 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {DxSelectBoxModule} from 'devextreme-angular';
-import {Estado} from '../../../interfaces/cidadeEstado/estado';
 import {DxButtonModule} from 'devextreme-angular/ui/button';
-import {Municipio} from '../../../interfaces/cidadeEstado/municipio';
 
 @Component({
   selector: 'app-cidade-estado',
