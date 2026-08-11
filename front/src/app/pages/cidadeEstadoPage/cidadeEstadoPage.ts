b4af47a6df8bccfa6524417d64cd111bcff2f63c
amanda <amandamarqueslinger@gmail.com>
2026-08-11 16:15:29 -0300

    ajuste no componente de estadoCidade

--- front/src/app/pages/cidadeEstadoPage/cidadeEstadoPage.ts
+++ front/src/app/pages/cidadeEstadoPage/cidadeEstadoPage.ts
@@ -1,58 +1,13 @@
-import { Component } from '@angular/core';
-import {CidadeEstado} from '../../shared/components/cidade-estado/cidade-estado';
-import {DxSelectBoxModule} from 'devextreme-angular';
-import {Estado} from '../../interfaces/cidadeEstado/estado';
-import {Municipio} from '../../interfaces/cidadeEstado/municipio';
-import {CidadeEstadoService} from '../../services/cidade-estado-service';
-import {DxButtonModule} from 'devextreme-angular/ui/button';
+import {Component} from '@angular/core';
+import {EstadoCidade} from '../../shared/components/estado-cidade/estado-cidade';
 
 @Component({
   selector: 'app-teste',
-  imports: [CidadeEstado, DxSelectBoxModule, DxButtonModule],
+  imports: [EstadoCidade],
   templateUrl: './cidadeEstadoPage.html',
   styleUrl: './cidadeEstadoPage.scss',
 })
 export class CidadeEstadoPage {
-
-  estados: Estado[] = [];
-  municipios:Municipio[] = [];
-
-  estadoEscolhido: any = null;
-
-  estadoSelecionado = '';
-  municipioSelecionado= '';
-
-  constructor(private cidadeEstadoService: CidadeEstadoService){}
-
-  ngOnInit() {
-    this.getEstados();
-  }
-
-  getEstados(){
-    this.cidadeEstadoService.listarEstados().subscribe({
-      next: (estados) => {
-        this.estados = estados;
-      },
-      error: (err) => console.error('Erro ao carregar estados', err),
-    });
-  }
-
-  aoSelecionarEstado(estado: {value?: string | null}): void {
-    this.estadoEscolhido = estado.value ?? null;
-
-    if(!this.estadoEscolhido){
-      this.estadoEscolhido = [];
-      return;
-    }
-
-    this.cidadeEstadoService.listarCidades(this.estadoEscolhido).subscribe(
-      (res: Municipio[] ) => {
-        this.municipios = res;
-      }
-    );
-  }
-
-  calculaFrete(){
-    console.log
-  }
+  estadoEscolhido: any;
+  municipioEscolhido: any;
 }
---
+++ front/src/app/shared/components/estado-cidade/estado-cidade.ts
@@ -0,0 +1,85 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {DxButtonComponent, DxSelectBoxComponent} from "devextreme-angular";
+import {CidadeEstadoService} from '../../../services/cidade-estado-service';
+import {Estado} from '../../../interfaces/cidadeEstado/estado';
+import {Municipio} from '../../../interfaces/cidadeEstado/municipio';
+
+@Component({
+  selector: 'app-estado-cidade',
+    imports: [
+        DxButtonComponent,
+        DxSelectBoxComponent
+    ],
+  templateUrl: './estado-cidade.html',
+  styleUrl: './estado-cidade.scss',
+})
+export class EstadoCidade {
+
+  estados: Estado[] = [];
+  municipios:Municipio[] = [];
+
+
+  @Input()
+  estadoSelecionado:any = {}
+
+  @Output()
+  estadoSelecionadoChange = new EventEmitter<Estado>();
+
+  @Input()
+  municipioSelecionado:any ={}
+
+  @Output()
+  municipioSelecionadoChange = new EventEmitter<Municipio>();
+
+
+  constructor(private cidadeEstadoService: CidadeEstadoService){}
+
+  ngOnInit() {
+    this.getEstados();
+  }
+
+  getEstados(){
+    this.cidadeEstadoService.listarEstados().subscribe({
+      next: (estados) => {
+        this.estados = estados;
+      },
+      error: (err) => console.error('Erro ao carregar estados', err),
+    });
+  }
+
+  aoSelecionarEstado(estado: any): void {
+    this.estadoSelecionado = estado.value ?? null;
+
+    console.log('Estado selecionado:', this.estadoSelecionado);
+
+    this.estadoSelecionadoChange.emit(this.estadoSelecionado);
+
+    if(!this.estadoSelecionado){
+      this.estadoSelecionado = [];
+      return;
+    }
+
+    this.cidadeEstadoService.listarCidades(this.estadoSelecionado.sigla).subscribe(
+      (res: Municipio[] ) => {
+        this.municipios = res;
+      }
+    );
+  }
+
+  aoSelecionarMunicipio(municipio: any){
+    this.municipioSelecionado = municipio.value ?? null;
+
+
+    console.log('Municipio selecionado:', this.municipioSelecionado);
+
+    this.municipioSelecionadoChange.emit(this.municipioSelecionado);
+
+    if(!this.municipioSelecionado){
+      this.municipioSelecionado = [];
+      return;
+    }
+  }
+  calculaFrete(){
+    console.log(this.municipioSelecionadoChange)
+  }
+}
